503d9629c7a85cd2bbf286e6f04966e886015aa3
Nomfundo Verah Sibiya <nvsibiya0@gmail.com>
2020-06-26 10:14:07 +0200

    changing port

--- services/services/server.js
+++ services/services/server.js
@@ -1,10 +1,15 @@
 const express = require("express")
+const listEndpoints = require("express-list-endpoints")
+const usersRoutes = require("./services/users")
+const usersRoutes = require("./services/movies")
 
 const server = express()
 
-const port = process.env.PORT
+const port = process.env.PORT 
 
-server.use(express.json())
+server.use(express.json()) //Built in middleware
+
+//Routes
 
 server.listen(port, () => {
     console.log('Server is running on port: ${port}') 
